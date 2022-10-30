845a85f7d5cc549ae3d1b5916f15ff672ff708fd
Sethukumar J <sethukumarj.76@gmail.com>
2022-10-30 11:16:19 +0530

    30 first commit

---
+++ Concurrency/channnel2.go
@@ -0,0 +1,63 @@
+package main 
+
+import (
+
+	"fmt"
+)
+
+
+
+
+func EvenInt (even chan int) {
+
+	var num = 2
+
+
+	for num <9 {
+		
+		even <- num
+
+		num = num+2
+	}
+	close(even)
+}
+
+
+func OddInt (odd chan int) {
+
+	var num = 1
+
+
+	for num <15 {
+		
+		odd <- num
+
+		num = num+2
+	}
+	close(odd)
+}
+
+
+
+func main() {
+ 
+	even := make(chan int)
+	odd := make(chan int)
+
+	go EvenInt(even)
+	go OddInt(odd)
+
+
+	for {
+ 
+		even , e := <- even
+		odd , o  := <- odd
+
+		if (e == false && o == false ) {
+			break
+		}
+
+		fmt.Println(even , e, odd, o)
+	}
+
+}
---
+++ ErronHandling/panic&recover2.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func main () {
+	chain(2)
+	chain(4)
+	chain(6)
+	chain(0)
+	chain(8)
+
+}
+
+var result = 1
+func chain(n int) {
+
+   
+	
+	defer func () {
+		if err:=recover(); err!=nil {
+			fmt.Println(err)
+		}
+	}()
+	if n ==0 {
+		panic(errors.New("O can't be used!"))
+	} else {
+		result *= n
+		fmt.Println(n)
+	}
+
+}
